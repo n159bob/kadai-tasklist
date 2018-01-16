4c416229f3f942cefdcb5cbcd0b8bbb72da20032
EC2 Default User <ec2-user@ip-172-31-9-231.ec2.internal>
2018-01-16 11:11:19 +0000

    kadai-tasklist CRUD

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = task.new
+  end
+
+  def create
+     @task = task.new(message_params)
+
+    if @task.save
+      flash[:success] = 'task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Message.find(params[:id])
+     if @task.update(message_params)
+      flash[:success] = 'task は正常に更新されました'
+      redirect_to @task
+     else
+      flash.now[:danger] = 'task は更新されませんでした'
+      render :edit
+     end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'task は正常に削除されました'
+    redirect_to task_url
+  end
+end
+
+ private
+
+# Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end
